90eb337b218672bedb70b32f3f14d2b34e82e64d
GaleGizmo <juanmiguelabelleira@gmail.com>
2025-06-27 10:17:27 +0200

    hotfix: ruta checkkeyword

--- src/routes/manualTasks.routes.js
+++ src/routes/manualTasks.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { runMorningTasks } = require("../../cronjobs");
-const { checkKeyword } = require('../../middleware/auth.js');
+const { checkKeyword } = require("../middleware/auth");
+
 
 router.post("/run-morning-tasks", checkKeyword, async (req, res) => {
   try {
